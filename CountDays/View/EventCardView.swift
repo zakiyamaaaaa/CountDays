104996686e77075f1c361f0b12833bc70eee29e5
zakiyamaaaaa <s.zakiyama@gmail.com>
2023-08-30 17:40:00 +0900

    イベントの時間更新ならびにTimelineViewを利用したViewの更新処理

--- CountDays/View/EventCardView.swift
+++ CountDays/View/EventCardView.swift
@@ -21,93 +21,94 @@ struct EventCardView: View {
     let width = UIScreen.main.bounds.width
     let timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
     
-    @State var currentSecond = 20
-    
     var body: some View {
-        ZStack {
-            let day = CalendarViewModel.getDates(target: date, eventType: eventType, frequentType: frequentType).days
-            let hour = CalendarViewModel.getDates(target: date, eventType: eventType, frequentType: frequentType).hours
-            let minute = CalendarViewModel.getDates(target: date, eventType: eventType, frequentType: frequentType).minutes
-            let second = CalendarViewModel.getDates(target: date, eventType: eventType, frequentType: frequentType).seconds
-            Rectangle()
-                .foregroundColor(backgroundColor.color)
-                .frame(width: width*1/3 + 10, height: width*1/3 + 10)
-                .cornerRadius(30)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 30)
-                        .stroke(.white, lineWidth: 3)
-                )
-            switch style {
-            case .standard:
-                VStack(alignment: .leading) {
-                    Text(title)
-                        .padding(.vertical, 10)
-                    Spacer()
-                    
-                    if second < 0 && eventType == .countdown && frequentType == .never {
-                        HStackLayout(alignment: .center) {
-                            Text("終了")
-                            
-                            Image(systemName: "checkmark.circle.fill")
-                        }
+        TimelineView(.periodic(from: date, by: 1)) { timeline in
+            ZStack {
+                
+                let day = CalendarViewModel.getDates(target: date, eventType: eventType, frequentType: frequentType).days
+                let hour = CalendarViewModel.getDates(target: date, eventType: eventType, frequentType: frequentType).hours
+                let minute = CalendarViewModel.getDates(target: date, eventType: eventType, frequentType: frequentType).minutes
+                let second = CalendarViewModel.getDates(target: date, eventType: eventType, frequentType: frequentType).seconds
+                
+                Rectangle()
+                    .foregroundColor(backgroundColor.color)
+                    .frame(width: width*1/3 + 10, height: width*1/3 + 10)
+                    .cornerRadius(30)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(.white, lineWidth: 3)
+                    )
+                
+                switch style {
+                case .standard:
+                    VStack(alignment: .leading) {
+                        Text(title)
+                            .padding(.vertical, 10)
                         Spacer()
-                    } else {
-                        Text("\(day)日")
-                            .font(.system(size: 30))
-                        HStack() {
-                            
-                            Text("\(hour)時間")
-                                .isHidden(hidden: !showHour)
-                            Text("\(minute)分")
-                                .isHidden(hidden: !showMinute)
-                            
-                        }
                         
-                        Text("\(currentSecond)秒")
-                            .onReceive(timer) { value in
-                                currentSecond = DateViewModel().getSecondNumber(date: value)
-                            }.isHidden(hidden: !showSecond)
-                    }
-                    
-                }
-                .foregroundColor(textColor.color)
-                .frame(width: width*1/3, height: width*1/3)
-                .padding()
-            case .circle:
-                VStack {
-                    Text(title)
-                        .foregroundColor(textColor.color)
-                    ZStack {
-                        
-                        Circle()
-                            .stroke(
-                                Color.pink.opacity(0.5),
-                                lineWidth: 10
-                            )
-                        Circle()
-                            .trim(from: 0, to: 0.25)
-                            .stroke(
-                                Color.pink,
-                                style: StrokeStyle(lineWidth: 10, lineCap: .round)
-                            )
-                        
-                        VStack {
-                            if second < 0 && eventType == .countdown && frequentType == .never {
+                        if second < 0 && eventType == .countdown && frequentType == .never {
+                            HStackLayout(alignment: .center) {
                                 Text("終了")
                                 
                                 Image(systemName: "checkmark.circle.fill")
-                            } else {
-                                Text("\(day)")
-                                    .font(.system(size: 30))
-                                    .fontWeight(.bold)
-                                Text("日")
                             }
+                            Spacer()
+                        } else {
+                            Text("\(day)日")
+                                .font(.system(size: 30))
+                            HStack() {
+                                
+                                Text("\(hour)時間")
+                                    .isHidden(hidden: !showHour)
+                                Text("\(minute)分")
+                                    .isHidden(hidden: !showMinute)
+                                
+                            }
+                            
+                            Text("\(second)秒")
+                                .isHidden(hidden: !showSecond)
                         }
-                        .rotationEffect(.degrees(90))
-                        .foregroundColor(textColor.color)
+                        
                     }
-                    .frame(width: width/5)
-                    .rotationEffect(.degrees(-90))
+                    .foregroundColor(textColor.color)
+                    .frame(width: width*1/3, height: width*1/3)
+                    .padding()
+                case .circle:
+                    VStack {
+                        Text(title)
+                            .foregroundColor(textColor.color)
+                        ZStack {
+                            
+                            Circle()
+                                .stroke(
+                                    Color.pink.opacity(0.5),
+                                    lineWidth: 10
+                                )
+                            Circle()
+                                .trim(from: 0, to: 0.25)
+                                .stroke(
+                                    Color.pink,
+                                    style: StrokeStyle(lineWidth: 10, lineCap: .round)
+                                )
+                            
+                            VStack {
+                                if second < 0 && eventType == .countdown && frequentType == .never {
+                                    Text("終了")
+                                    
+                                    Image(systemName: "checkmark.circle.fill")
+                                } else {
+                                    Text("\(day)")
+                                        .font(.system(size: 30))
+                                        .fontWeight(.bold)
+                                    Text("日")
+                                }
+                            }
+                            .rotationEffect(.degrees(90))
+                            .foregroundColor(textColor.color)
+                        }
+                        .frame(width: width/5)
+                        .rotationEffect(.degrees(-90))
+                    }
                 }
             }
         }
@@ -116,6 +117,6 @@ struct EventCardView: View {
 
 struct EventCardView_Previews: PreviewProvider {
     static var previews: some View {
-        EventCardView(title: "sanoke1", date: EventCardViewModel.defaultStatus.date, style: .standard, backgroundColor: .primary, textColor: .white, frequentType: .annual, eventType: .countdown)
+        EventCardView(title: "sanoke1", date: EventCardViewModel.defaultStatus.date, style: .standard, backgroundColor: .primary, textColor: .white, showSecond: true, frequentType: .annual, eventType: .countdown)
     }
 }
