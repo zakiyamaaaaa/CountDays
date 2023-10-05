8884258371f2d98d695aebee46812bc87731ef74
zakiyamaaaaa <s.zakiyama@gmail.com>
2023-10-05 19:48:32 +0900

    Widgetの更新頻度をアップデート、ウィジェットのダークモードで色変わる問題修正

---
+++ CountDays/View/Sandbox/TestView5.swift
@@ -0,0 +1,18 @@
+//
+//  TestView5.swift
+//  CountDays
+//
+//  Created by shoichiyamazaki on 2023/10/04.
+//
+
+import SwiftUI
+
+struct TestView5: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView5()
+}
