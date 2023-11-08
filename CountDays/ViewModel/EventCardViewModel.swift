406f28d143c261d2f6d6f53a147182f0bba77194
taberulayu <example@com.com>
2023-11-08 19:17:48 +0900

    デフォルトの時刻を修正

--- CountDays/ViewModel/EventCardViewModel.swift
+++ CountDays/ViewModel/EventCardViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 struct EventCardViewModel {
     
     static let defaultStatus = Event(title: "", date:
-                                        Date().addingTimeInterval(-160)
+                                        Date().addingTimeInterval(16000)
 //                                        Calendar(identifier: .gregorian).date(from: DateComponents(year: 2023, month: 10, day: 8, hour: 0,minute: 0,second: 0)) ?? Date()
                                      , textColor: .white, backgroundColor: .blue, displayStyle: .standard, fontSize: 1.0, frequentType: .never, eventType: .countdown, dayAtMonthly: 10, dayOfWeek: .thursday)
 }
